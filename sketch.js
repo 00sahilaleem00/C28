1ed22c8c3f36c8e3b52f5aa77946063e719755a2
00sahilaleem00 <00sahilaleem00@gmail.com>
2020-09-08 16:47:27 +0530

    bob

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+const Body = Matter.Body;
 
 var engine, world;
 var box1, pig1;
@@ -39,7 +40,7 @@ function setup(){
     bird = new Bird(100,100);
 
     log6 = new Log(230,180,80, PI/2);
-    chain = new Chain(bird.body,log6.body);
+    slingshot = new Slingshot(bird.body,{x:300, y:100});
 }
 
 function draw(){
@@ -64,5 +65,16 @@ function draw(){
     bird.display();
     platform.display();
     log6.display();
-    chain.display();    
+    slingshot.display();    
+
+
 }
+
+function mouseDragged(){
+    Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+}
+
+function mouseReleased(){
+    slingshot.release();
+}
+
